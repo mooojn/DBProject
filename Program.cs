839c7d9795281f9d6f821b0b9bf8bfc57f945d86
mooojn <moontariq7867@gmail.com>
2024-03-18 20:46:14 +0500

    Add

--- Program.cs
+++ Program.cs
@@ -11,29 +11,15 @@ namespace DBProject
     {
         public static string connectionString = "Data Source=DESKTOP-AJHCE58\\MOOOJN;Initial Catalog=ProjectB;Integrated Security=True";
         public static SqlConnection connection = new SqlConnection(connectionString);
-        public static int RegNo = 0;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            RegNo = GetRegNo();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Uet());
-        }
-        static int GetRegNo()
-        {
-            connection.Open();
-            string query = "SELECT RegistrationNumber From Student " +
-                            "Where id = (SELECT MAX(id) FROM Student)";
-            SqlCommand cmd = new SqlCommand(query, connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-            reader.Read();
-            int regNo = Convert.ToInt32(reader["RegistrationNumber"]);
-            connection.Close();
-            return ++regNo;
         }
     }
 }
