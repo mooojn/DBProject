9ed082b6d2260c3288f9852de7fa88bc60c76fb9
mooojn <moontariq7867@gmail.com>
2024-03-18 20:46:08 +0500

    Data Loading and Insertion

--- Main/Students.Designer.cs
+++ Main/Students.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button4 = new System.Windows.Forms.Button();
             this.addStudentPanel = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,24 +40,15 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.addStudentPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(443, 474);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(210, 75);
-            this.button4.TabIndex = 29;
-            this.button4.Text = "View All Students";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click_1);
-            // 
             // addStudentPanel
             // 
+            this.addStudentPanel.Controls.Add(this.label5);
+            this.addStudentPanel.Controls.Add(this.textBox5);
             this.addStudentPanel.Controls.Add(this.label4);
             this.addStudentPanel.Controls.Add(this.label3);
             this.addStudentPanel.Controls.Add(this.label2);
@@ -66,15 +58,31 @@
             this.addStudentPanel.Controls.Add(this.textBox3);
             this.addStudentPanel.Controls.Add(this.textBox1);
             this.addStudentPanel.Controls.Add(this.button6);
-            this.addStudentPanel.Location = new System.Drawing.Point(150, 12);
+            this.addStudentPanel.Location = new System.Drawing.Point(25, 27);
             this.addStudentPanel.Name = "addStudentPanel";
-            this.addStudentPanel.Size = new System.Drawing.Size(571, 456);
+            this.addStudentPanel.Size = new System.Drawing.Size(499, 384);
             this.addStudentPanel.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 25);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "RegNo";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(203, 245);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(241, 31);
+            this.textBox5.TabIndex = 19;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(94, 263);
+            this.label4.Location = new System.Drawing.Point(41, 199);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 25);
             this.label4.TabIndex = 18;
@@ -83,7 +91,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(93, 197);
+            this.label3.Location = new System.Drawing.Point(37, 140);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 25);
             this.label3.TabIndex = 17;
@@ -92,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 125);
+            this.label2.Location = new System.Drawing.Point(34, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(115, 25);
             this.label2.TabIndex = 16;
@@ -101,7 +109,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 52);
+            this.label1.Location = new System.Drawing.Point(35, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 15;
@@ -109,35 +117,35 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(262, 263);
+            this.textBox4.Location = new System.Drawing.Point(203, 193);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(241, 31);
             this.textBox4.TabIndex = 14;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(262, 191);
+            this.textBox2.Location = new System.Drawing.Point(203, 135);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(241, 31);
             this.textBox2.TabIndex = 13;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(262, 119);
+            this.textBox3.Location = new System.Drawing.Point(203, 80);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(241, 31);
             this.textBox3.TabIndex = 12;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(262, 46);
+            this.textBox1.Location = new System.Drawing.Point(203, 25);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(241, 31);
             this.textBox1.TabIndex = 11;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(229, 336);
+            this.button6.Location = new System.Drawing.Point(154, 299);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(168, 65);
             this.button6.TabIndex = 10;
@@ -145,36 +153,26 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(96, 475);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(213, 75);
-            this.button8.TabIndex = 27;
-            this.button8.Text = "Add New Student";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, -2);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 522);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(788, 453);
-            this.dataGridView1.TabIndex = 30;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1417, 333);
+            this.dataGridView1.TabIndex = 31;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // Students
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(797, 579);
+            this.ClientSize = new System.Drawing.Size(1417, 855);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.addStudentPanel);
-            this.Controls.Add(this.button8);
             this.Name = "Students";
             this.Text = "Students";
             this.Load += new System.EventHandler(this.Students_Load);
@@ -186,7 +184,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Panel addStudentPanel;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
@@ -197,7 +194,8 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Main/Students.cs
+++ Main/Students.cs
@@ -20,15 +20,20 @@ namespace DBProject
 
         private void Students_Load(object sender, EventArgs e)
         {
-            addStudentPanel.Hide();
-            dataGridView1.Hide();
+            loadData();   
+        }
+        private void loadData()
+        {
+            Program.connection.Open();
+            string query = "SELECT * FROM Student";
+            SqlDataAdapter sda = new SqlDataAdapter(query, Program.connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            Program.connection.Close();
         }
 
-        private void button8_Click(object sender, EventArgs e)
-        {
-            addStudentPanel.Show();
-            dataGridView1.Hide();
-        }
+        
 
         private void button6_Click(object sender, EventArgs e)
         {
@@ -42,35 +47,33 @@ namespace DBProject
             cmd.Parameters.AddWithValue("@L_Name", textBox2.Text);
             cmd.Parameters.AddWithValue("@Contact", textBox3.Text);
             cmd.Parameters.AddWithValue("@Email", textBox4.Text);
-            cmd.Parameters.AddWithValue("@RegNo", Program.RegNo);
-            Program.RegNo++;
+            cmd.Parameters.AddWithValue("@RegNo", textBox5.Text);
 
             cmd.ExecuteNonQuery();
 
             Program.connection.Close();
-            MessageBox.Show("Data has been inserted");
+            //MessageBox.Show("Data has been inserted");
+            loadData();
         }
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-            addStudentPanel.Hide();
-            dataGridView1.Show();
-            Program.connection.Open();
-            string query = "SELECT RegistrationNumber AS RegNo," +
-                "CONCAT(FirstName, ' ' , LastName) AS FullName," +
-                "Contact FROM Student";
-            SqlDataAdapter sda = new SqlDataAdapter(query, Program.connection);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-
-            dataGridView1.DataSource = dt;
-
-            Program.connection.Close();
+        
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = dataGridView1.SelectedRows[0].DataBoundItem;
+            textBox1.Text = ((DataRowView)row).Row.ItemArray[1].ToString();
+            textBox2.Text = ((DataRowView)row).Row.ItemArray[2].ToString();
+            textBox3.Text = ((DataRowView)row).Row.ItemArray[3].ToString();
+            textBox4.Text = ((DataRowView)row).Row.ItemArray[4].ToString();
+            textBox5.Text = ((DataRowView)row).Row.ItemArray[0].ToString();
         }
     }
 }
