e92890cf6ec4fd8930a56aa04eaed67c537ac971
mooojn <moontariq7867@gmail.com>
2024-03-19 16:42:16 +0500

    File Name Change

---
+++ Main/Clo.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DBProject
+{
+    partial class Clo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(70, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 185);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 313);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(935, 246);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 75);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Clos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 559);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Clos";
+            this.Text = "Clos";
+            this.Load += new System.EventHandler(this.Clos_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Main/Clo.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace DBProject
+{
+    public partial class Clo : Form
+    {
+        public Clo()
+        {
+            InitializeComponent();
+            loadData();
+        }
+        private void loadData()
+        {
+            Program.connection.Open();
+            string query = "SELECT * FROM CLO";
+            SqlDataAdapter sda = new SqlDataAdapter(query, Program.connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            // Converting date+time from dateUpdated coloumn to date only
+            dt.AsEnumerable().ToList().ForEach(row =>
+            {
+                if (row["DateUpdated"] != DBNull.Value)
+                {
+                    row["DateUpdated"] = ((DateTime)row["DateUpdated"]).ToShortDateString();
+                }
+            });
+
+            dataGridView1.DataSource = dt;
+            Program.connection.Close();
+        }
+        private void Clos_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Program.connection.Open();
+            string query = "INSERT INTO CLO VALUES (@Name, @DateCreated, @DateCreated)";   // 3rd val for DateUpdated
+
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@Name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@DateCreated", DateTime.Now);
+            cmd.ExecuteNonQuery();
+
+            Program.connection.Close();
+            loadData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+
+        
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = dataGridView1.SelectedRows[0].DataBoundItem;
+            textBox1.Text = ((DataRowView)row).Row.ItemArray[1].ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Program.connection.Open();
+            string name = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+
+
+            string query = "UPDATE CLO SET Name = @NewName," +
+                "DateUpdated = @NewTime WHERE Name = @OldName";
+
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@OldName", name);
+            cmd.Parameters.AddWithValue("@NewName", textBox1.Text);
+            cmd.Parameters.AddWithValue("@NewTime", DateTime.Now);
+            int changes = cmd.ExecuteNonQuery();
+
+            if (changes > 0)
+                MessageBox.Show("Data has been updated");
+            else
+                MessageBox.Show("Data not found");
+            Program.connection.Close();
+            loadData();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            Program.connection.Open();
+            string query = "DELETE FROM CLO WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            Program.connection.Close();
+            loadData();
+
+        }
+    }
+}
