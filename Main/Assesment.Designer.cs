22e651e1830a348ff9a065f0c954b4b986744d60
mooojn <moontariq7867@gmail.com>
2024-03-19 20:52:52 +0500

    Add Assessment Functionality

--- Main/Assesment.Designer.cs
+++ Main/Assesment.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DBProject.Main
+﻿namespace DBProject
 {
     partial class Assesment
     {
@@ -28,32 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 256);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 194);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(438, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 66);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 66);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Total Weightage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Total Marks";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(219, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(265, 31);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(219, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(265, 31);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 31);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Title";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 77);
+            this.button1.Location = new System.Drawing.Point(79, 172);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 67);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 66);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(568, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 66);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Add Component";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Assesment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Assesment";
             this.Text = "Assesment";
+            this.Load += new System.EventHandler(this.Assesment_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Main/Assesment.cs
+++ Main/Assesment.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace DBProject.Main
+namespace DBProject
 {
     public partial class Assesment : Form
     {
         public Assesment()
         {
             InitializeComponent();
+        }
+
+        private void Assesment_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.connection.Open();
+            string query = "INSERT INTO Assessment values" +
+                "(@Title, @DateCreated, @TotalMarks, @TotalWeightage)";
+            SqlCommand command = new SqlCommand(query, Program.connection);
+            command.Parameters.AddWithValue("@Title", textBox1.Text);    
+            command.Parameters.AddWithValue("@DateCreated", DateTime.Now);
+            command.Parameters.AddWithValue("@TotalMarks", textBox2.Text);
+            command.Parameters.AddWithValue("@TotalWeightage", textBox3.Text);
+            command.ExecuteNonQuery();
+            Program.connection.Close();
+            
+            loadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+            int id = Convert.ToInt32(selectedRow.Cells[0].Value);   
+
+            Program.connection.Open();
+            string query = "UPDATE Assessment SET Title = @Title, TotalMarks = @TotalMarks," +
+                " TotalWeightage = @TotalWeightage " +
+                "WHERE Id = @Id";
+            
+            SqlCommand command = new SqlCommand(query, Program.connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.Parameters.AddWithValue("@Title", textBox1.Text);
+            command.Parameters.AddWithValue("@TotalMarks", textBox2.Text);    
+            command.Parameters.AddWithValue("@TotalWeightage", textBox3.Text);
+            command.ExecuteNonQuery();
+            
+            Program.connection.Close();
+            loadData();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = dataGridView1.SelectedRows[0];
+            textBox1.Text = row.Cells[1].Value.ToString();
+            textBox2.Text = row.Cells[3].Value.ToString();
+            textBox3.Text = row.Cells[4].Value.ToString();
+        }
+        private void loadData()
+        {
+            Program.connection.Open();
+            string query = "SELECT * FROM Assessment";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, Program.connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            Program.connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+            Program.connection.Open();
+            string query = "DELETE FROM Assessment WHERE Id = @Id";
+            SqlCommand command = new SqlCommand(query, Program.connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+            Program.connection.Close();
+            loadData();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AssesmentComponent form = new AssesmentComponent();
+            form.Show();
         }
     }
 }
