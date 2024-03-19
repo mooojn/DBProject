f4a49c5ee7c54d88bc7650aa3c144b1d78c8b382
mooojn <moontariq7867@gmail.com>
2024-03-19 20:54:00 +0500

    Add Assessment and Attendance func

---
+++ Main/Sub/AssesmentComponent.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DBProject
+{
+    partial class AssesmentComponent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AssessmentNameComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.RubricNameComboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 262);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 188);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // AssessmentNameComboBox
+            // 
+            this.AssessmentNameComboBox.FormattingEnabled = true;
+            this.AssessmentNameComboBox.Location = new System.Drawing.Point(162, 104);
+            this.AssessmentNameComboBox.Name = "AssessmentNameComboBox";
+            this.AssessmentNameComboBox.Size = new System.Drawing.Size(244, 33);
+            this.AssessmentNameComboBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Assessment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(437, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total Marks";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rubric";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(579, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 31);
+            this.textBox2.TabIndex = 8;
+            // 
+            // RubricNameComboBox
+            // 
+            this.RubricNameComboBox.FormattingEnabled = true;
+            this.RubricNameComboBox.Location = new System.Drawing.Point(579, 107);
+            this.RubricNameComboBox.Name = "RubricNameComboBox";
+            this.RubricNameComboBox.Size = new System.Drawing.Size(188, 33);
+            this.RubricNameComboBox.TabIndex = 9;
+            this.RubricNameComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(535, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AssesmentComponent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.RubricNameComboBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AssessmentNameComboBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "AssesmentComponent";
+            this.Text = "AssesmentComponent";
+            this.Load += new System.EventHandler(this.AssesmentComponent_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox AssessmentNameComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox RubricNameComboBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Main/Sub/AssesmentComponent.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+namespace DBProject
+{
+    public partial class AssesmentComponent : Form
+    {
+        public AssesmentComponent()
+        {
+            InitializeComponent();
+        }
+
+        private void AssesmentComponent_Load(object sender, EventArgs e)
+        {
+            loadData();
+            loadAssessmentData();
+            loadRubricData();
+        }
+        private void loadRubricData()
+        {
+            RubricNameComboBox.Items.Clear();
+            Program.connection.Open();
+            string query = "SELECT Details FROM Rubric";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            SqlDataReader data = cmd.ExecuteReader();
+            while (data.Read())
+            {
+                RubricNameComboBox.Items.Add(data.GetString(0));
+            }
+            Program.connection.Close();
+        }
+        private void loadAssessmentData()
+        {
+            AssessmentNameComboBox.Items.Clear();
+            Program.connection.Open();
+            string query = "SELECT Title FROM Assessment";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            SqlDataReader data = cmd.ExecuteReader();
+            while (data.Read())
+            {
+                AssessmentNameComboBox.Items.Add(data.GetString(0));
+            }
+            Program.connection.Close();
+        }
+        private void loadData()
+        {
+            Program.connection.Open();
+            string query = "SELECT * FROM AssessmentComponent";
+                
+            SqlDataAdapter SDA = new SqlDataAdapter(query, Program.connection);
+                
+            DataTable data = new DataTable();
+            SDA.Fill(data);
+            dataGridView1.DataSource = data;
+            Program.connection.Close();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int assessmentId = getAssessmentId();
+            int rubricId = getRubricId();
+
+            Program.connection.Open();
+            string query = "INSERT INTO AssessmentComponent VALUES " +
+                "(@Name, @RubricId, @TotalMarks, @DateCreated, @DateUpdated, @AssessmentId)";
+            
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            
+            cmd.Parameters.AddWithValue("@Name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@RubricId", rubricId);
+            cmd.Parameters.AddWithValue("@TotalMarks", textBox2.Text);
+            cmd.Parameters.AddWithValue("@DateCreated", DateTime.Now);
+            cmd.Parameters.AddWithValue("@DateUpdated", DateTime.Now);
+            cmd.Parameters.AddWithValue("@AssessmentId", assessmentId);
+            cmd.ExecuteNonQuery();
+            
+            Program.connection.Close();
+            loadData();
+        }
+        private int getAssessmentId()
+        {
+            int id = -1;
+            Program.connection.Open();
+            string query = "SELECT Id FROM Assessment WHERE Title = @Title";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@Title", AssessmentNameComboBox.Text);
+            SqlDataReader data = cmd.ExecuteReader();
+            while (data.Read())
+            {
+                id = data.GetInt32(0);
+            }
+            Program.connection.Close();
+
+            return id;
+        }
+        private int getRubricId()
+        {
+            int id = -1;
+            Program.connection.Open();
+            string query = "SELECT Id FROM Rubric WHERE Details = @Detail";
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@Detail", RubricNameComboBox.Text);
+            SqlDataReader data = cmd.ExecuteReader();
+            while (data.Read())
+            {
+                id = data.GetInt32(0);
+            }
+            Program.connection.Close();
+
+            return id;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int assessmentId = getAssessmentId();
+            int rubricId = getRubricId();
+            Program.connection.Open();
+            string query = "UPDATE AssessmentComponent SET " +
+                "Name = @Name, RubricId = @RubricId, TotalMarks = @TotalMarks, " +
+                "DateUpdated = @DateUpdated, AssessmentId = @AssessmentId WHERE Id = @Id";
+            
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@Name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@RubricId", rubricId);
+            cmd.Parameters.AddWithValue("@TotalMarks", textBox2.Text);
+            cmd.Parameters.AddWithValue("@DateUpdated", DateTime.Now);
+            cmd.Parameters.AddWithValue("@AssessmentId", assessmentId);
+            cmd.Parameters.AddWithValue("@Id", dataGridView1.SelectedRows[0].Cells[0].Value);
+
+            cmd.ExecuteNonQuery();
+            Program.connection.Close();
+            loadData();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var rows = dataGridView1.SelectedRows[0];
+            textBox1.Text = rows.Cells[1].Value.ToString();
+            textBox2.Text = rows.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Program.connection.Open();
+            string query = "DELETE FROM AssessmentComponent WHERE Id = @Id";
+
+            SqlCommand cmd = new SqlCommand(query, Program.connection);
+            cmd.Parameters.AddWithValue("@Id", dataGridView1.SelectedRows[0].Cells[0].Value);
+
+            cmd.ExecuteNonQuery();
+            Program.connection.Close();
+            loadData();
+        }
+    }
+}
