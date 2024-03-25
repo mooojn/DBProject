0f8c75f08616934aaea8efefc6785f9d70fa8498
mooojn <moontariq7867@gmail.com>
2024-03-25 21:10:16 +0500

    Add load reg boc

--- Main/Sub/Attendance.cs
+++ Main/Sub/Attendance.cs
@@ -21,8 +21,8 @@ namespace DBProject
 
         private void Attendance_Load(object sender, EventArgs e)
         {
-            loadRegNoComboBox();
-            loadStatusComboBox();
+            QueryDL.LoadComboBox(RegNoComboBox, "RegistrationNumber", "Student");
+            QueryDL.LoadComboBox(StatusComboBox, "TOP 4 *", "Lookup");
         }
         private void insertIntoclassAttendance()
         {
@@ -49,55 +49,18 @@ namespace DBProject
                 return;
             }
             insertIntoclassAttendance();
-            
             // getting the ids
             int attendanceId = QueryDL.GetIdFromTable("MAX(Id)", "ClassAttendance");
             int stdId = QueryDL.GetIdFromTableUsingString("Id", "Student", "RegistrationNumber", RegNoComboBox.Text);
             int status = QueryDL.GetIdFromTableUsingString("Lookupid", "Lookup", "Name", StatusComboBox.Text);
-            
             Program.connection.Open();
-            string query = "INSERT INTO StudentAttendance VALUES (@id, @StdId, @AttendanceStatus)";
-            
+
+            string query = $"INSERT INTO StudentAttendance VALUES ({attendanceId}, {stdId}, {status})";
             SqlCommand cmd = new SqlCommand(query, Program.connection);
-            cmd.Parameters.AddWithValue("@id", attendanceId);    /// change id to auto increment
-            cmd.Parameters.AddWithValue("@StdId", stdId);
-            cmd.Parameters.AddWithValue("@AttendanceStatus", status);
             cmd.ExecuteNonQuery();
 
             Program.connection.Close();
-
             MessageBox.Show("Attendance Marked", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-        
-        private void loadRegNoComboBox()
-        {
-            Program.connection.Open();
-
-            string query = "SELECT RegistrationNumber FROM Student";
-
-            SqlCommand cmd = new SqlCommand(query, Program.connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read()) {
-                RegNoComboBox.Items.Add(reader["RegistrationNumber"]);
-            }
-            Program.connection.Close();
-        }
-        private void loadStatusComboBox()
-        {
-            StatusComboBox.Items.Clear();
-
-            Program.connection.Open();
-
-            string query = "SELECT TOP 4 * FROM Lookup";
-
-            SqlCommand cmd = new SqlCommand(query, Program.connection);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read()) {
-                StatusComboBox.Items.Add(reader.GetString(1));
-            }
-            Program.connection.Close();
         }
         private bool textBoxIsNull()
         {
